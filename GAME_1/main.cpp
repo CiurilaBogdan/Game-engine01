96eec1b7a9216a21852f4f11b80a90fd5617735b
Ciurila Bogdan <bogdanciurila95@gmail.com>
2019-12-17 12:45:41 +0200

    added file loading to shader class

--- GAME_1/main.cpp
+++ GAME_1/main.cpp
@@ -1,6 +1,8 @@
 #include <glew.h>
 #include <glfw3.h>
 #include <cstdio>
+#include <fstream>
+#include <string>
 
 #include "src/graphics/shader.h"
 
@@ -9,12 +11,66 @@ using namespace engine1;
 
 int main(void)
 {
+	/*ifstream file_to_read;
+	string fileTxt = "";
+	
+	file_to_read.open("GoodFile.txt",std::ios_base::in);
+	
+	if (file_to_read.is_open()) {
+		
+		while (file_to_read.good()) {
+			
+			getline(file_to_read, fileTxt);
+
+		}
+		file_to_read.close();
+	}
+
+	printf(fileTxt.c_str());*/
 
 
+	//second attempt
+
+	/*FILE* file;
+	char buffer[256];
+	
+	fopen_s(&file, "GoodFile.txt", "r");
+	
+	if (file == nullptr) {
+		printf("ERROR\n");
+	}
+	else {
+
+	}
+	
+
+	fclose(file);
+	
+	printf(buffer);*/
+
+	FILE* f;
+	fopen_s(&f,"GoodFile.txt", "rt");
+
+	// Determine file size
+	fseek(f, 0, SEEK_END);
+	unsigned long size = ftell(f);
+	
+	char* where = new char[size+1];
+	memset(where, 0, size + 1);
+
+	rewind(f);
+	fread(where, sizeof(char), size, f);
+
+	//process file and break it into different shaders
+	string shad(where);
+	 
+	const char* shader_vertex_source = shad.c_str();
+	delete[] where;
+	
 	//read from file
 	
 
-	return 0;
+
 	float triangle[] = {
 	-0.5f, -0.5f, 
 	 0.5f, -0.5f, 
@@ -51,12 +107,12 @@ int main(void)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
 
-	const char* shader_vertex_source = R"(#version 330 core
+	/*const char* shader_vertex_source = R"(#version 330 core
 		layout(location = 0) in vec3 aPos;
 		void main()
 	{
 		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-	})";
+	})";*/
 
 	
 	
